508af0825965d5835fdb0b5832b4615a424a2b5e
Konstantin <verbov777@gmail.com>
2021-04-29 19:52:55 +0300

    Commit 1

---
+++ Test/Func.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * Created by Ranorex
+ * User: kverbov
+ * Date: 03.12.2014
+ * Time: 12:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace Test
+{
+	/// <summary>
+	/// Description of Func.
+	/// </summary>
+	public static class Func
+	{
+		/// <summary>
+        /// Holds an instance of the TestRepository repository.
+        /// </summary>
+        public static TestRepository repo = TestRepository.Instance;
+		
+		/// <summary>
+		/// следующий шаг или конец боя
+		/// </summary>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+		/// <returns>следующий шаг</returns>
+		public static bool WaitNextStep(Duration min,  Duration max)
+        {
+        	while(!Validate_Вперёд())
+        	{
+        		//	если видна кнопка "Обновить"
+        		if(repo.БойцовскийКлуб1.Menu.ОбновитьInfo.Exists() && repo.БойцовскийКлуб1.Menu.Обновить.Visible)
+        		{
+            		Report.Log(ReportLevel.Info, "Keyboard",  "Процесс боя. Жмем кнопку 'Обновить'.");
+            		Func.ClickElement(repo.БойцовскийКлуб1.Menu.Обновить);
+            		Wait(min,max);
+            		continue;
+            	}         
+        		else if(repo.БойцовскийКлуб1.Menu.ВернутьсяInfo.Exists() && repo.БойцовскийКлуб1.Menu.Вернуться.Visible)
+            	{
+            		Report.Log(ReportLevel.Info, "Keyboard", "Процесс боя. Конец боя. Нажатие кнопки 'Вернуться'.");
+            		Func.ClickElement(repo.БойцовскийКлуб1.Menu.Вернуться);
+            		return false;
+            	}   
+        		else if(repo.БойцовскийКлуб1.Menu.Combats1Info.Exists() && repo.БойцовскийКлуб1.Menu.Combats1.Visible)
+        		{
+        			Report.Log(ReportLevel.Info, "Keyboard", "Процесс боя. Появилась кнопка 'Поединки'. Конец боя.");
+        			return false;
+
+        		}
+        		else 
+            	{
+        			Keyboard.Press("{F5}");
+        			Report.Log(ReportLevel.Info, "Keyboard", "Процесс боя. Ничего не появилось. Жмем 'F5'.");
+        			continue;
+        			
+            		
+//            		return false;
+            	}
+        	}
+        	
+        	//	появилась кнопка "Вперед"
+        	return true;
+        	      
+        }
+        
+		/// <summary>
+		/// Ждем
+		/// </summary>
+		/// <param name="min"></param>
+		/// <param name="max"></param>
+        public static void Wait(Ranorex.Duration min, Ranorex.Duration max)
+        {
+        	Random R=new Random();
+        	int time=R.Next(min,max);
+        	Report.Log(ReportLevel.Info, "Keyboard", "Ждем в течение "+time.ToString());
+        	Thread.Sleep(time);        	
+        	
+        }
+
+        public static bool ClickElement(WebElement item)
+        {
+        	if(item.EnsureVisible() && item.Valid)
+        	{
+        		Report.Log(ReportLevel.Info, "Клик на элемент "+item.ToString() +". Элемент найден. Делаем клик.");
+        		Validate.Exists(item);
+        		item.PerformClick();// Mouse.Click( item);
+        		Delay.Seconds(2);
+        		return true;
+        	}
+        	else
+        	{
+        		Report.Log(ReportLevel.Info, "Клик на элемент "+item.ToString() +". Элемент не найден!");
+        		return false;
+        	}
+        	
+        }
+        
+        public static bool WaitElementAppear(Element item)
+        {
+        	if(item.EnsureVisible() && item.Valid && item.ScreenRectangle.Size.Width!=0)
+        	{
+        		Report.Log(ReportLevel.Info, "Проверка видимости элемента "+item.ToString() +". Элемент найден.");
+        		return true;
+        	}
+        	else
+        	{
+        		Report.Log(ReportLevel.Info, "Проверка видимости элемента "+item.ToString() +". Элемент не найден!");
+        		return false;
+        	}
+        	
+        }
+        
+        public static bool Validate_Вперёд()
+        {
+        	
+        	if(repo.БойцовскийКлуб1.Menu.ВперёдInfo.Exists() &&repo.БойцовскийКлуб1.Menu.Вперёд.Visible&& repo.БойцовскийКлуб1.Menu.Вперёд.ScreenRectangle.Size.Width!=0)        	
+            {
+        		Report.Log(ReportLevel.Info, "Keyboard", "Кнопка 'Вперед' появилась.");
+            	return true;
+            }
+            else
+            {
+            	Report.Log(ReportLevel.Info, "Keyboard", "Кнопка 'Вперед' не появилась.");
+            	return false;
+            	
+            }            	
+        }
+	}
+}
---
+++ Test/Program.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Created by Ranorex
+ * User: kverbov
+ * Date: 01.12.2014
+ * Time: 15:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Threading;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Reporting;
+using Ranorex.Core.Testing;
+using SD=System.Diagnostics;
+
+namespace Test
+{
+    class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            // Uncomment the following 2 lines if you want to automate Windows apps
+            // by starting the test executable directly
+            //if (Util.IsRestartRequiredForWinAppAccess)
+            //    return Util.RestartWithUiAccess();
+
+            Keyboard.AbortKey = System.Windows.Forms.Keys.Pause;
+            int error = 0;
+			try{
+			
+			Launch.Start();
+			//Launch.Instance.RepairPerson();
+			while(true)
+            {					
+	            Launch.Instance.ChooseFight();
+	            System.DateTime start=System.DateTime.Now;
+	            
+	            //	если групповой бой
+	            if(Launch.Instance.fightType!="1")            		
+	            {
+	            //	если противники не набраны, то бой отменяется автоматически
+	            if(!Launch.Instance.WaitGPFight(10000,45000,6*60000))
+	            continue;            			
+	            }
+	            else
+	            Launch.Instance.WaitFight(10000,45000);
+	            	
+	            Fight.Instance.DoFight(false);
+	            Report.Log(ReportLevel.Info,"Закрываем браузер.");
+	            Host.Local.KillBrowser("ie");
+	            Func.Wait(105000,195000);
+	            Launch.Start();            		
+            }
+            				
+            error = TestSuiteRunner.Run(typeof(Program), Environment.CommandLine);
+            }
+            catch (Exception e)
+            {
+                Report.Error("Unexpected exception occurred: " + e.ToString());
+                error = -1;
+            }
+            return error;
+        }
+        
+        
+        
+        
+    }
+}
